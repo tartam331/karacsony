0ccb8f8d1d765525f824775e2f3a05f32d0921dd
tartam331 <tartam331@hengersor.hu>
2024-12-18 10:34:34 +0100

    vegleges

--- java.js
+++ java.js
@@ -1,5 +1,6 @@
 let szavazatok = {};
 $(document).ready(function() {
+    // Szavazás gombok eseménykezelője
     $('.voteBtn').click(function() {
         let giftId = $(this).data('id');
         
@@ -7,12 +8,35 @@ $(document).ready(function() {
             szavazatok[giftId] = 0;
         }
         szavazatok[giftId]++;
-
+        
+        // Frissítjük a szavazatot a megfelelő kártyán
         $('#vote-' + giftId).text(szavazatok[giftId] + ' szavazat');
+
+        // Frissítjük a legnagyobb szavazatot
+        updateMaxVote();
     });
+
+    // Kezdeti legnagyobb szavazat frissítése
+    updateMaxVote();
 });
 
-function showStory() {
-    $('#story').toggle();
-}
+// Funkció, ami frissíti a legnagyobb szavazatot
+function updateMaxVote() {
+    let maxVote = 0;
+    let maxVoteGiftId = null;
 
+    // Végigmegyünk a szavazatokon, hogy megtaláljuk a legnagyobbat
+    for (let giftId in szavazatok) {
+        if (szavazatok[giftId] > maxVote) {
+            maxVote = szavazatok[giftId];
+            maxVoteGiftId = giftId;
+        }
+    }
+
+    // A legnagyobb szavazattal rendelkező ajándék megjelenítése
+    if (maxVoteGiftId !== null) {
+        $('#max-vote').text('A legtöbb szavazatot a "' + $('#vote-' + maxVoteGiftId).closest('.card').find('.card-title').text() + '" kapta (' + maxVote + ' szavazat)');
+    } else {
+        $('#max-vote').text('Még nincs szavazat.');
+    }
+}

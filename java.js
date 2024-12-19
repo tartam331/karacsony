8184a072703c5c03b08759915253412329eff390
tartam331 <tartam331@hengersor.hu>
2024-12-19 12:10:25 +0100

    vegleges karacsonyi 12.18

--- java.js
+++ java.js
@@ -48,3 +48,72 @@ function updateMaxVote() {
         $('#max-vote').text('Még nincs szavazat.');
     }
 }
+
+
+$(document).ready(function () {
+    let szavazatok = {};
+
+    // Új ajándék hozzáadása
+    $('#add-gift-form').submit(function (event) {
+        event.preventDefault();
+
+        const giftName = $('#gift-name').val();
+        const giftImage = $('#gift-image').val();
+        const newGiftId = Object.keys(szavazatok).length + 1;
+
+        if (giftName && giftImage) {
+            // Ajándék hozzáadása a DOM-hoz
+            $('#gift-container').append(`
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="${giftImage}" class="card-img-top" alt="Ajándék ${newGiftId}">
+                        <div class="card-body">
+                            <h5 class="card-title">${giftName}</h5>
+                            <button class="btn btn-primary voteBtn" data-id="${newGiftId}">Erre szavazok</button>
+                            <p class="vote-count" id="vote-${newGiftId}">0 szavazat</p>
+                        </div>
+                    </div>
+                </div>
+            `);
+
+            // Szavazatok inicializálása
+            szavazatok[newGiftId] = 0;
+
+            // Gomb eseménykezelő hozzáadása
+            $(`.voteBtn[data-id='${newGiftId}']`).click(function () {
+                const giftId = $(this).data('id');
+
+                szavazatok[giftId]++;
+                $(`#vote-${giftId}`).text(`${szavazatok[giftId]} szavazat`);
+
+                updateMaxVote();
+            });
+
+            // Form mezők törlése
+            $('#gift-name').val('');
+            $('#gift-image').val('');
+        } else {
+            alert('Kérlek töltsd ki mindkét mezőt!');
+        }
+    });
+
+    function updateMaxVote() {
+        let maxVote = 0;
+        let maxVoteGiftId = null;
+
+        for (let giftId in szavazatok) {
+            if (szavazatok[giftId] > maxVote) {
+                maxVote = szavazatok[giftId];
+                maxVoteGiftId = giftId;
+            }
+        }
+
+        if (maxVoteGiftId !== null) {
+            $('#max-vote').text(
+                `A legtöbb szavazatot a "${$(`#vote-${maxVoteGiftId}`).closest('.card').find('.card-title').text()}" kapta (${maxVote} szavazat)`
+            );
+        } else {
+            $('#max-vote').text('Még nincs szavazat.');
+        }
+    }
+});
